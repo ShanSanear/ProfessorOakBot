bcded6e9749f4118780b6a30f90286f9c9fb803d
Shan <pruskimateusz93@gmail.com>
2025-10-06 19:21:08 +0200

    Added missing logging

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,3 @@
-from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -14,8 +12,9 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# DISABLED: This interferes with bot's logging configuration
+# if config.config_file_name is not None:
+#     fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
